39cd97d5acabf017ebc65e4a8fefde297ac4a40f
Joel Palmer <joelpalmer@gmail.com>
2021-08-02 04:31:09 -0500

    add more TS object query maps

--- public dots/vim-nvim/lua/joel/config.lua
+++ public dots/vim-nvim/lua/joel/config.lua
@@ -68,6 +68,47 @@ require("nvim-treesitter.configs").setup {
     },
     -- highlight_current_scope = {enable = true}
   },
+  textobjects = {
+    lsp_interop = {
+      enable = true,
+      border = "none",
+      peek_definition_code = {
+        ["df"] = "@function.outer",
+        ["dF"] = "@class.outer",
+      },
+    },
+    move = {
+      enable = true,
+      set_jumps = true, -- whether to set jumps in the jumplist
+      goto_next_start = {
+        ["]m"] = "@function.outer",
+        ["]]"] = "@class.outer",
+      },
+      goto_next_end = {
+        ["]M"] = "@function.outer",
+        ["]["] = "@class.outer",
+      },
+      goto_previous_start = {
+        ["[m"] = "@function.outer",
+        ["[["] = "@class.outer",
+      },
+      goto_previous_end = {
+        ["[M"] = "@function.outer",
+        ["[]"] = "@class.outer",
+      },
+    },
+    -- @TODOUA: these selectors may or may not helpful workflow
+    select = {
+      enable = true,
+      lookahead = true,
+      keymaps = {
+        ["af"] = "@function.outer",
+        ["if"] = "@function.inner",
+        ["ac"] = "@class.outer",
+        ["ic"] = "@class.inner",
+      },
+    },
+  },
 }
 
 -- colorizer
