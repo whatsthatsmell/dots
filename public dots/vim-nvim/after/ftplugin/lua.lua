72004721815f8b29fefb7c85d6a8d291c03d4910
Joel Palmer <joelpalmer@gmail.com>
2021-10-08 09:24:10 -0500

    lua ftp file migrated to lua

--- public dots/vim-nvim/after/ftplugin/lua.lua
+++ public dots/vim-nvim/after/ftplugin/lua.lua
@@ -1,6 +1,9 @@
-setlocal textwidth=120
-setlocal shiftwidth=2
-setlocal colorcolumn=121
+vim.opt_local.textwidth = 120
+vim.opt_local.shiftwidth = 2
+vim.opt_local.colorcolumn = "121"
+vim.api.nvim_exec(
+  [[
+
 setlocal formatoptions-=o
 " source the file - 
 nmap <silent><localleader>1 :luafile%<cr>
@@ -14,24 +17,6 @@ autocmd CursorMoved,InsertLeave,BufEnter,BufWinEnter,TabEnter,BufWritePost *
 \ lua require'lsp_extensions'.inlay_hints{ prefix = '', highlight = "Comment", enabled = {"TypeHint", "ChainingHint", "ParameterHint"} }
 " Show diagnostic popup on cursor hold but don't steal cursor
 autocmd CursorHold * lua vim.diagnostic.show_line_diagnostics({focusable = false})
-" Setup cmp source buffer configuration (nvim-lua source only enables in Lua filetype)
-autocmd FileType lua lua require'cmp'.setup.buffer {
-\   sources = {
-\     { name = 'nvim_lua' },
-\     { name = 'nvim_lsp' },
-\     { name = 'treesitter' },
-\     { name = 'vsnip' },
-\     {
-\      name = 'buffer',
-\      opts = {
-\        get_bufnrs = function()
-\          return vim.api.nvim_list_bufs()
-\        end,
-\      },
-\    },
-\    { name = 'path' },
-\   },
-\ }
 
 " snippets for Lua - TODO: change autoselect next completion?
 let b:vsnip_snippet_dir = expand('~/.config/nvim/snippets/')
@@ -43,3 +28,26 @@ nnoremap <silent> g] <cmd>lua vim.lsp.diagnostic.goto_next()<CR>
 sign define DiagnosticSignHint text= texthl=DiagnosticSignHint linehl= numhl=
 sign define DiagnosticSignWarn text= texthl=DiagnosticSignWarn linehl= numhl=
 sign define DiagnosticSignError text= texthl=DiagnosticSignError linehl= numhl=
+]],
+  false
+)
+
+-- Setup cmp source buffer configuration (nvim-lua source only enables in Lua filetype)
+local cmp = require "cmp"
+cmp.setup.buffer {
+  sources = {
+    { name = "nvim_lua" },
+    { name = "nvim_lsp" },
+    { name = "treesitter" },
+    { name = "vsnip" },
+    {
+      name = "buffer",
+      opts = {
+        get_bufnrs = function()
+          return vim.api.nvim_list_bufs()
+        end,
+      },
+    },
+    { name = "path" },
+  },
+}
