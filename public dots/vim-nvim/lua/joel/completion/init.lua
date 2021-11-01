3daa38e51d141d6e1a56fd934fb0477529b9556e
Joel Palmer <joelpalmer@gmail.com>
2021-11-01 13:45:03 -0500

    cmp maps and cmdline setup seem ok

--- public dots/vim-nvim/lua/joel/completion/init.lua
+++ public dots/vim-nvim/lua/joel/completion/init.lua
@@ -1,17 +1,20 @@
 -- Setup nvim-cmp
 local cmp = require "cmp"
 
--- try cmp-cmdline completion for /search
--- @TODOUA: try ':' later
--- cmdline_buffer src for space handling in cmd search '/'...
--- -- it's a temp solution from: https://github.com/hrsh7th/nvim-cmp/issues/417
--- -- I have mixed feelings about cmdline_buffer in its current form. Watching above for fleshed out src.
-cmp.register_source("cmdline_buffer", require("cmp_buffer").new())
+-- cmp in cmdline is a WIP - I mostly use telescope to navigate anyway
 cmp.setup.cmdline("/", {
   sources = cmp.config.sources {
-    -- { name = "cmdline_buffer", opts = { keyword_pattern = [=[[^[:blank:]].*]=] } },
     { name = "buffer" },
   },
+})
+
+-- Use cmdline & path source for ':'.
+cmp.setup.cmdline(":", {
+  sources = cmp.config.sources({
+    { name = "path" },
+  }, {
+    { name = "cmdline" },
+  }),
 })
 
 local lspkind = require "lspkind"
@@ -26,15 +29,13 @@ cmp.setup {
     ["<C-d>"] = cmp.mapping.scroll_docs(-4),
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
     ["<C-Space>"] = cmp.mapping.complete(),
-    ["<CR>"] = cmp.mapping.confirm { select = true },
-    -- Right is for ghost_text to behave like terminal
-    ["<Right>"] = cmp.mapping.confirm { select = true },
-    -- Don't insert if I explicitly exit
-    -- Don't insert if I explicitly exit
-    -- Start completion manually with C-Space to have it truly clean-up
+    ["<CR>"] = cmp.mapping {
+      i = cmp.mapping.confirm { select = true },
+      c = cmp.mapping.confirm { select = false },
+    },
+    ["<Tab>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "s" }),
+    ["<S-Tab>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "s" }),
     ["<C-e>"] = cmp.mapping.abort(),
-    -- Insert instead of Select so you don't go away at `stopinsert` after `CursorHoldI`
-    -- @TODOUA: I want to be able to `Select` without `stopinsert` killing it (& keep `stopinsert`)
     ["<Up>"] = cmp.mapping(cmp.mapping.select_prev_item { behavior = cmp.SelectBehavior.Insert }, { "i", "c" }),
     ["<Down>"] = cmp.mapping(cmp.mapping.select_next_item { behavior = cmp.SelectBehavior.Insert }, { "i", "c" }),
   },
