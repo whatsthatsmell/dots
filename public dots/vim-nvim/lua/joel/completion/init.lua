73b7a59dfedcecccccdd405f40dced932e9e55e0
Joel Palmer <joelpalmer@gmail.com>
2021-11-18 16:48:16 -0600

    cmp autopairs parens

--- public dots/vim-nvim/lua/joel/completion/init.lua
+++ public dots/vim-nvim/lua/joel/completion/init.lua
@@ -101,3 +101,7 @@ cmp.setup {
     end,
   },
 }
+
+-- insert `(` after select function or method item
+local cmp_autopairs = require "nvim-autopairs.completion.cmp"
+cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done { map_char = { tex = "" } })
