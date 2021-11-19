c1c7247256ff05b0b72482ffa88d13fb24b9cfe3
Joel Palmer <joelpalmer@gmail.com>
2021-11-19 04:36:36 -0600

    1805679 fixes gitsigns blame_line

--- public dots/vim-nvim/lua/joel/config.lua
+++ public dots/vim-nvim/lua/joel/config.lua
@@ -165,29 +165,6 @@ require("gitsigns").setup {
     },
   },
   numhl = true,
-  keymaps = {
-    -- Default keymap options
-    -- temp until this is impl'd: https://github.com/lewis6991/gitsigns.nvim/commit/5e49bb09f324580519d1ef41cb03bcb07536a239
-    noremap = true,
-
-    ["n ]c"] = { expr = true, "&diff ? ']c' : '<cmd>lua require\"gitsigns.actions\".next_hunk()<CR>'" },
-    ["n [c"] = { expr = true, "&diff ? '[c' : '<cmd>lua require\"gitsigns.actions\".prev_hunk()<CR>'" },
-
-    ["n <leader>hs"] = '<cmd>lua require"gitsigns".stage_hunk()<CR>',
-    ["v <leader>hs"] = '<cmd>lua require"gitsigns".stage_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
-    ["n <leader>hu"] = '<cmd>lua require"gitsigns".undo_stage_hunk()<CR>',
-    ["n <leader>hr"] = '<cmd>lua require"gitsigns".reset_hunk()<CR>',
-    ["v <leader>hr"] = '<cmd>lua require"gitsigns".reset_hunk({vim.fn.line("."), vim.fn.line("v")})<CR>',
-    ["n <leader>hR"] = '<cmd>lua require"gitsigns".reset_buffer()<CR>',
-    ["n <leader>hp"] = '<cmd>lua require"gitsigns".preview_hunk()<CR>',
-    ["n <leader>hb"] = '<cmd>lua require"gitsigns".blame_line{full=true}<CR>',
-    ["n <leader>hS"] = '<cmd>lua require"gitsigns".stage_buffer()<CR>',
-    ["n <leader>hU"] = '<cmd>lua require"gitsigns".reset_buffer_index()<CR>',
-
-    -- Text objects
-    ["o ih"] = ':<C-U>lua require"gitsigns.actions".select_hunk()<CR>',
-    ["x ih"] = ':<C-U>lua require"gitsigns.actions".select_hunk()<CR>',
-  },
 }
 
 -- treesitter
