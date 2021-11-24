6385808b2ebd7d5e83646cd13bbbb07e0b514fb0
Joel Palmer <joelpalmer@gmail.com>
2021-11-24 05:22:51 -0600

    change TS inc sel maps

--- public dots/vim-nvim/lua/joel/treesitter/init.lua
+++ public dots/vim-nvim/lua/joel/treesitter/init.lua
@@ -3,10 +3,10 @@ require("nvim-treesitter.configs").setup {
   incremental_selection = {
     enable = true,
     keymaps = {
-      init_selection = "gnn",
-      node_incremental = "grn",
-      scope_incremental = "grc",
-      node_decremental = "grm",
+      init_selection = "<CR>",
+      scope_incremental = "<CR>",
+      node_incremental = "<TAB>",
+      node_decremental = "<S-TAB>",
     },
   },
   indent = { enable = true },
