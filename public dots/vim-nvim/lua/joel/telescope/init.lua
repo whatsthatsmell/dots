9af035bcc0173ca35cc1e0ca93c6e8f44cae6e36
Joel Palmer <joelpalmer@gmail.com>
2021-11-30 17:02:48 -0600

    properly working multi-select telescope

--- public dots/vim-nvim/lua/joel/telescope/init.lua
+++ public dots/vim-nvim/lua/joel/telescope/init.lua
@@ -8,10 +8,15 @@ function custom_actions._multiopen(prompt_bufnr, open_cmd)
   local picker = action_state.get_current_picker(prompt_bufnr)
   local num_selections = #picker:get_multi_selection()
   if num_selections > 1 then
-    local picker = action_state.get_current_picker(prompt_bufnr)
+    local cwd = picker.cwd
+    if cwd == nil then
+      cwd = ""
+    else
+      cwd = string.format("%s/", cwd)
+    end
     vim.cmd "bw!"
     for _, entry in ipairs(picker:get_multi_selection()) do
-      vim.cmd(string.format("%s %s", open_cmd, entry.value))
+      vim.cmd(string.format("%s %s%s", open_cmd, cwd, entry.value))
     end
     vim.cmd "stopinsert"
   else
@@ -269,7 +274,7 @@ function M.find_configs()
       "~/.config/nvim",
       "~/.config/alacritty",
     },
-    cwd = "~/.config/nvim/",
+    -- cwd = "~/.config/nvim/",
     layout_strategy = "horizontal",
     layout_config = { preview_width = 0.65, width = 0.75 },
   }
