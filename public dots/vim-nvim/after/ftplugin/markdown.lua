8d34b74558a88c35f706adf4c041aa2c05926626
Joel Palmer <joelpalmer@gmail.com>
2021-12-15 05:29:19 -0600

    toggle md ts hl and wrap word with md link

--- public dots/vim-nvim/after/ftplugin/markdown.lua
+++ public dots/vim-nvim/after/ftplugin/markdown.lua
@@ -12,6 +12,12 @@ vim.opt_local.conceallevel = 2
 -- For Glow, just type :Glow, I almost never use the :MarkdownPreview keymap, I type it.
 vim.api.nvim_buf_set_keymap(0, "n", ",md", "<Plug>MarkdownPreview", { noremap = false })
 
+-- toggle TS highlighting for markdown
+vim.api.nvim_buf_set_keymap(0, "n", ",th", ":TSBufToggle highlight<CR>", { noremap = false })
+
+-- wrap selection in markdown link
+vim.api.nvim_buf_set_keymap(0, "v", ",wl", [[c[<c-r>"]()<esc>>]], { noremap = false })
+
 vim.api.nvim_exec(
   [[
 " arrows
