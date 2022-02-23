556581b0dafad6c49e3042776cb4cec81f2041ec
Joel Palmer <joelpalmer@gmail.com>
2022-02-23 03:29:20 -0600

    add update crate map to toml ftp

--- public dots/vim-nvim/after/ftplugin/toml.lua
+++ public dots/vim-nvim/after/ftplugin/toml.lua
@@ -1,3 +1,14 @@
 require("crates").setup()
 local cmp = require "cmp"
 cmp.setup.buffer { sources = { { name = "crates" }, name = "buffer" } }
+
+-- crates mappings
+-- upgrade crate on line
+vim.api.nvim_buf_set_keymap(
+  0,
+  "n",
+  ",uc",
+  [[<cmd>lua require('crates').upgrade_crate()<cr>
+]],
+  { noremap = true, silent = true }
+)
