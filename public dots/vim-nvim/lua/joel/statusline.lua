a5368225639d7170ec3d8dbaee6c086cf7c6736c
Joel Palmer <joelpalmer@gmail.com>
2022-03-18 05:18:33 -0500

    GLOBAL STATUS GOODNESS

--- public dots/vim-nvim/lua/joel/statusline.lua
+++ public dots/vim-nvim/lua/joel/statusline.lua
@@ -46,6 +46,7 @@ require("lualine").setup {
     section_separators = { left = "", right = "" },
     disabled_filetypes = {},
     always_divide_middle = false,
+    globalstatus = true,
   },
   sections = {
     lualine_a = { "mode" },
