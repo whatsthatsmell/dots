1f4a41665e060bf1e964721dae3c9eeb24e1e2b8
Joel Palmer <joelpalmer@gmail.com>
2022-06-02 06:26:10 -0500

    bump nvim

--- public dots/vim-nvim/init.lua
+++ public dots/vim-nvim/init.lua
@@ -1,5 +1,5 @@
 -- Always on bleeding edge Neovim from https://git.io/NeovimHEAD --
--- NVIM v0.8.0-dev+344-g5d840fa7e
+-- NVIM v0.8.0-dev+357-gd93ba03c7
 
 -- https://github.com/lewis6991/impatient.nvim
 -- :LuaCacheClear
